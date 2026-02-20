4ae44f90cc12a95a3a2a317cb35b77f0b020c565
asanchez365365 <saintxotic123@gmail.com>
2026-02-20 08:20:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,109 @@
-#AaronSanchez
-#Playlist Organizer  
-# List of songs, function to filter by genre or mood.
+# Hospital Check-in Program
+# Author: Jafet Perez Reyes & Aaron Sanchez
+# Date: 2/10/2026
 
-playlist = []
-# this list stores songs as dictionaries (song name +genre/mood)
 
-def show_playlist(song_list, chosen_category):
+# this is where we're going to store this
+patients = [
+    {
+    "Name": "Alex",
+    "Age": 67,
+    "Gender": "Both",
+    "Date of Birth": "6/07/67",
+    "Symptoms": "Pain",
+    "Priority": "low"
+    },
+    {"Name": "Aex",
+    "Age": 67,
+    "Gender": "Both",
+    "Date of Birth": "6/07/67",
+    "Symptoms": "Pain",
+    "Priority": "low"
+    }
+    ]
 
-    found= False # checks and makes sure if a matching song exists
+
+
+
+
+def search_patient(Name):
     
+    for patient in patients: # for every dictionary in patients
+        if patient["Name"] == Name: # if the name value in the dictionary is equal to the paramenter "Name"
+                print()
+                print("Patient found:")
 
-    for song in song_list: 
-        # this is iteration that goes through each song
-        name = song["name"]
-        category = song["category"]
+                for key, value in patient.items(): # for every key and value in the items of the idctionary "patient"
+                    print(f"{key}: {value}") # do this (printing the item's key and value)
 
-        if category.lower() == chosen_category.lower():
-            # checking if categroy matches using selection
-            print("Song:", name)
-            print("Genre/Mood", category)
-            print()
-            found=True
+                return # stop this function
+    print("Patient not found.") # print if no found bc if it is the function will stop
 
-    if found == False:
-        print("No songs found")
-        # This is just in case no songs are found
 
-print ("Welcome to your Playlist Organizer!")
+
+
+ 
+# the variable patient is a local varible so its only defined inside the fuction
+
+
+
+
+
+def add_patient(): 
+    patient = { # our empty list
+        "Name": None,
+        "Age": None, 
+        "Gender": None,
+        "Date of Birth": None,
+        "Symptoms": None,
+        "Priority": None
+        }
+    
+    for key in patient: # for every key in our paitent list
+        patient[key] = input(key + ": ") # print that and get input
+
+    for dictionaries in patients: # the dictionaries we have in this
+        if patient["Name"] == dictionaries["Name"]: # if the thing we entered is in the databse
+            print("Patient already exsists")
+        else: #otherwise
+            patients.append(patient) # add to database
+
+            print(f"Patient {patient["Name"]}, has been successfully added")
+            return
+
+
+
+
+# We're going to have a main loop
+# We're going to use "break" to get out of loop when when user wants to exit.
+
+
+
+
 
 while True:
-
     print()
-    print("1 Add a song")
-    print("2 View songs by genre or mood")
-    print("3 Exit")
+    print("==== Hospital Patient Check-in ====")
+    print("1. Add Patient")
+    print("2. Lookup Patient")
+    print("3. Exit")
 
-    choice= input("Choose option: ")
+    try: # try this
+        user_input = int(input())
+        if user_input == 1:
 
-    #to add a song...
+            add_patient()
 
-    if choice == "1":
-        song_name = input("Please enter song name :")
-        song_category = input ("Enter genre or mood")
+        elif user_input == 2:
 
-        song_data = {
-            "name": song_name,
-            "category": song_category
-        }
+            patient_name = input("Enter the patients name: ")
+            search_patient(patient_name)
 
-        playlist.append(song_data)
-        print("Song added.")
+        elif user_input == 3:
 
-    #now we shall view the songs
-    elif choice == "2":
-        if len(playlist) == 0:
-            print("Playlist is empty")
-        else:
-            search = input("Enter genre or mood:")
-            show_playlist(playlist, search)
+            print("Goodbye!")
+            break
 
-    elif choice == "3":
-        print("Goodbye for now.")
-        break
-
-    else:
-        print("Invalid choice")
+    except ValueError: # if we get "value error"
+        print()
+        print("Please enter a number.")
